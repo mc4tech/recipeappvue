9ff5840dbdacbb8035dd9ddd1b38cac57335d4ec
mc4tech <chavezmeguel@yahoo.com>
2019-03-08 19:53:11 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RecipeList />
+  </div>
+</template>
+
+<script>
+import RecipeList from "@/components/RecipeList.vue";
+
+export default {
+  name: "app",
+  components: {
+    RecipeList
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/RecipeItem.vue
@@ -0,0 +1,66 @@
+<template>
+  <li class="summary-card">
+    <div class="tn">
+      <img :src="recipe.image" />
+    </div>
+    <div class="recipe-meta">
+      <h2>{{recipe.label}}</h2>
+      <p>{{recipe.calories | addCals}}</p>
+      <button @click="showDetails">Show Details</button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "recipe-item",
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  },
+  filters: {
+    addCals: function(calories) {
+      return calories.toFixed(0) + " calories";
+    }
+  },
+  methods: {
+    showDetails() {
+      this.$emit("show-details", this.recipe);
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+button {
+  cursor: pointer;
+}
+
+.summary-card {
+  display: flex;
+}
+
+.tn {
+  display: inline-flex;
+  vertical-align: middle;
+}
+
+.tn img {
+  max-height: 200px;
+  margin: auto;
+}
+
+.recipe-meta {
+  flex-grow: 1;
+  padding-left: 20px;
+  text-align: right;
+}
+
+.recipe-meta h2 {
+  text-align: left;
+  color: #009688;
+  font-weight: bold;
+}
+</style>
